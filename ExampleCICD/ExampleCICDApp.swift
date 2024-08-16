9f2d6544941b4727c0598125036548c3febb4cc5
far1h <farihmuhammad45@gmail.com>
2024-08-16 23:05:24 +0700

    Initial Commit

---
+++ ExampleCICD/ExampleCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleCICDApp.swift
+//  ExampleCICD
+//
+//  Created by Farih Muhammad on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
